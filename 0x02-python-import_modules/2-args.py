17a5674986e4b89b67b6ff9143d08cad8e2f0c4c
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-10-03 15:41:17 +0000

    debug task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,7 @@ if __name__ == '__main__':
             print('{:d}: {}'.format(i, arg_vector[i]))
     elif len_argv == 1:
         print(len_argv, 'argument:')
+        for i in range(1, len_argv + 1):
+            print('{:d}: {}'.format(i, arg_vector[i]))
     elif len_argv == 0:
         print(len_argv, 'arguments.')
