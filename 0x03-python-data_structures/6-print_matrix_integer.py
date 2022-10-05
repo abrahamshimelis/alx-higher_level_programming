3b190555b3a469fecf64cb801c65d793a7f2c1c5
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-10-05 16:18:21 +0000

    debug task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,5 +9,5 @@ def print_matrix_integer(matrix=[[]]):
                     print('{:d}'.format(element), end='')
                 else:
                     print('{:d}'.format(element), end=' ')
-                i +=1
+                    i += 1
             print()
