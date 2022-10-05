583ab4805b0cea63efecec8060822245d6c03b31
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-10-05 16:40:38 +0000

    debug task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_b = check_tuple(tuple_b)
 
     return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+
+
 def check_tuple(my_tuple=()):
     if len(my_tuple) < 2:
         if len(my_tuple) == 1:
