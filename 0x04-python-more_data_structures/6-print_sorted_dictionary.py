f9f4a1bb376c4c893909094298dc0d7b97f90377
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-10-15 14:21:46 +0000

    debug task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dictionary = sorted(a_dictionary.items())
     for k, v in sorted_dictionary:
-        print("{0} : {1}".format(k, v))
+        print("{0}: {1}".format(k, v))
